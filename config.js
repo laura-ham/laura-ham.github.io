5f8d5ab06eba2cf7a2c2a2bd86941e36b464bec3
laura-ham <laura-ham@hotmail.com>
2019-11-25 17:07:02 +0200

    updated visualization

--- config.js
+++ config.js
@@ -1,24 +1,22 @@
 var CONFIG = {
  "data": {
-  "_lastModified": 1574669510729,
-  "assetsPath": "/tmp/.mount_GapminmmFr90/resources/preview-data/",
-  "ddfPath": "./data/ddf--gapminder--systema_globalis",
-  "lastModified": 1574669510729,
-  "path": "./data/ddf--gapminder--systema_globalis",
-  "reader": "ddf"
- },
- "data_carbon_footprint2.csv": {
-  "_lastModified": 1574669510729,
-  "chartType": "BubbleChart",
-  "format": "csv",
+  "_lastModified": 1574687696960,
   "hasNameColumn": true,
-  "lastModified": 1574669510729,
+  "lastModified": 1574687696960,
   "nameColumnPosition": 0,
+  "noCache": true,
   "path": "./data/carbon_footprint2.csv",
   "reader": "ext-csv",
   "sheet": "",
   "timeInColumns": false,
   "ddfPath": "./data/carbon_footprint2.csv"
+ },
+ "data_ddf--gapminder--systema_globalis": {
+  "_lastModified": 1574687696960,
+  "lastModified": 1574687696960,
+  "path": "./data/ddf--gapminder--systema_globalis",
+  "reader": "ddf",
+  "ddfPath": "./data/ddf--gapminder--systema_globalis"
  },
  "locale": {
   "filePath": "assets/translation/",
@@ -33,11 +31,7 @@ var CONFIG = {
     "type": "entity_domain"
    },
    "dim": "country",
-   "filter": {
-    "country": {
-     "un_state": true
-    }
-   },
+   "filter": {},
    "show": {},
    "showFallback": {},
    "showItemsMaxCount": null,
@@ -73,7 +67,7 @@ var CONFIG = {
      "index": 0,
      "type": "measure"
     },
-    "data": "data_carbon_footprint2.csv",
+    "data": "data",
     "domainMax": null,
     "domainMin": null,
     "fixBaseline": null,
@@ -99,7 +93,7 @@ var CONFIG = {
      "index": 1,
      "type": "measure"
     },
-    "data": "data_carbon_footprint2.csv",
+    "data": "data",
     "domainMax": null,
     "domainMin": null,
     "fixBaseline": null,
@@ -123,7 +117,7 @@ var CONFIG = {
      ]
     },
     "autoconfig": {},
-    "data": "data_carbon_footprint2.csv",
+    "data": "data",
     "palette": {
      "0": "#8c30e8",
      "25": "#30a3e8",
@@ -153,7 +147,7 @@ var CONFIG = {
     "data": "data",
     "scaleType": "ordinal",
     "syncModels": [],
-    "use": "property",
+    "use": "indicator",
     "which": "name"
    },
    "limit": 5000,
@@ -162,33 +156,78 @@ var CONFIG = {
    "opacitySelectDim": 0.3,
    "select": [
     {
-     "country": "col",
+     "country": "zwe",
+     "labelOffset": [
+      0.243,
+      -0.139
+     ],
+     "trailStartTime": "1961"
+    },
+    {
+     "country": "zmb",
+     "labelOffset": [
+      0.243,
+      -0.046
+     ],
+     "trailStartTime": "1961"
+    },
+    {
+     "country": "yem",
+     "labelOffset": [
+      0.216,
+      0.018
+     ],
+     "trailStartTime": "1961"
+    },
+    {
+     "country": "ven",
+     "labelOffset": [
+      0.204,
+      0.079
+     ],
+     "trailStartTime": "1961"
+    },
+    {
+     "country": "vnm",
+     "labelOffset": [
+      0.243,
+      0.037
+     ],
      "trailStartTime": "1961"
     },
     {
      "country": "fin",
+     "labelOffset": [
+      -0.211,
+      -0.058
+     ],
      "trailStartTime": "1961"
     }
    ],
    "size": {
     "allow": {
      "scales": [
-      "linear"
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
      ]
     },
     "autoconfig": {
      "index": 2,
      "type": "measure"
     },
-    "data": "data",
-    "domainMax": 1400000000,
-    "domainMin": 15,
+    "data": "data_ddf--gapminder--systema_globalis",
+    "domainMax": null,
+    "domainMin": null,
     "extent": [
      0,
      0.85
     ],
     "fixBaseline": 0,
     "scaleType": "linear",
+    "spaceRef": null,
     "syncModels": [],
     "use": "indicator",
     "which": "population_total",
@@ -234,29 +273,29 @@ var CONFIG = {
    "allowSelectMultiple": true,
    "highlight": [],
    "hook_geoshape": {
-    "data": "data",
+    "data": "data_ddf--gapminder--systema_globalis",
     "scaleType": "ordinal",
     "syncModels": [],
     "use": "property",
     "which": "shape_lores_svg"
    },
    "hook_rank": {
-    "data": "data",
+    "data": "data_ddf--gapminder--systema_globalis",
     "scaleType": "linear",
     "syncModels": [],
     "use": "property",
     "which": "rank"
    },
    "label": {
-    "data": "data",
+    "data": "data_ddf--gapminder--systema_globalis",
     "scaleType": "ordinal",
     "syncModels": [],
     "use": "property",
     "which": "name"
    },
    "limit": 1000,
-   "opacityHighlightDim": 0.3,
-   "opacityRegular": 0.8,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
    "opacitySelectDim": 0.3,
    "select": [],
    "space": [
@@ -288,7 +327,7 @@ var CONFIG = {
    "playing": false,
    "record": false,
    "round": "round",
-   "showForecast": false,
+   "showForecast": true,
    "start": "1961",
    "startOrigin": null,
    "startSelected": "1961",
@@ -313,34 +352,7 @@ var CONFIG = {
   "chart": {
    "decorations": {
     "enabled": true,
-    "xAxisGroups": {
-     "income_per_person_gdppercapita_ppp_inflation_adjusted": [
-      {
-       "label": "incomegroups/level1",
-       "label_short": "incomegroups/level1short",
-       "max": 2650,
-       "min": null
-      },
-      {
-       "label": "incomegroups/level2",
-       "label_short": "incomegroups/level2short",
-       "max": 8000,
-       "min": 2650
-      },
-      {
-       "label": "incomegroups/level3",
-       "label_short": "incomegroups/level3short",
-       "max": 24200,
-       "min": 8000
-      },
-      {
-       "label": "incomegroups/level4",
-       "label_short": "incomegroups/level4short",
-       "max": null,
-       "min": 24200
-      }
-     ]
-    }
+    "xAxisGroups": null
    },
    "labels": {
     "dragging": true,
@@ -351,7 +363,7 @@ var CONFIG = {
     "left": 80.37,
     "top": 0
    },
-   "showForecastOverlay": true,
+   "showForecastOverlay": false,
    "superhighlightOnMinimapHover": true,
    "trails": true,
    "whenHovering": {
@@ -363,16 +375,8 @@ var CONFIG = {
   },
   "cursorMode": "arrow",
   "datawarning": {
-   "doubtDomain": [
-    1800,
-    1950,
-    2015
-   ],
-   "doubtRange": [
-    1,
-    0.3,
-    0.2
-   ]
+   "doubtDomain": [],
+   "doubtRange": []
   },
   "dialogs": {
    "dialog": {
@@ -411,13 +415,6 @@ var CONFIG = {
   "show_ticks": true,
   "sidebarCollapse": false,
   "splash": false,
-  "treemenu": {
-   "folderStrategyByDataset": {
-    "data": "spread",
-    "data_fasttrack": "spread",
-    "data_wdi": "folder:other_datasets"
-   }
-  },
   "zoomOnScrolling": false
  },
  "chartType": "BubbleChart"
